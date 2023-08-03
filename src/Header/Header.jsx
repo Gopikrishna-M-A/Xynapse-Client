0b9035ab4b17d60d1eb70cf7abb21f205e5e9846
Gopikrishna-M-A <84523270+Gopikrishna-M-A@users.noreply.github.com>
2023-08-03 11:38:19 +0530

    cdn

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-// import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 export default function Header() {
   const scrollToSection = (sectionId) => {
